49b81ed919d0144239eb8fac870f5a2e9d1d7569
Carlos Fonseca M <carlfm01@hotmail.com>
2019-01-04 00:08:11 -0600

    Add iOS exercises

---
+++ IOS/Fireworks/Fireworks/Fireworks/ViewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UIKit;
+
+namespace Fireworks
+{
+    public partial class ViewController : UIViewController
+    {
+        
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+            OpenAboutButton.TouchUpInside += OpenAboutButton_TouchUpInside;
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        private void OpenAboutButton_TouchUpInside(object sender, EventArgs e)
+        {
+            AboutViewController aboutVC = (AboutViewController)Storyboard.InstantiateViewController("AboutViewController");
+
+            PresentViewController(aboutVC, true, null);
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ IOS/TipCalculator/TipCalculator/MyViewController.cs
@@ -0,0 +1,46 @@
+﻿using CoreGraphics;
+using UIKit;
+
+namespace TipCalculator
+{
+    public class MyViewController : UIViewController
+    {
+        public MyViewController()
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            View.BackgroundColor = UIColor.Yellow;
+            var totalAmount = new UITextField()
+            {
+                Frame = new CGRect(20, 28, View.Bounds.Width - 40, 35),
+                KeyboardType = UIKeyboardType.DecimalPad,
+                BorderStyle = UITextBorderStyle.RoundedRect,
+                Placeholder = "Enter Total Amount",
+            };
+            var calcButton = new UIButton(UIButtonType.Custom)
+            {
+                Frame = new CGRect(20, 71, View.Bounds.Width - 40, 45),
+                BackgroundColor = UIColor.FromRGB(0, 0.5f, 0),
+            };
+            calcButton.SetTitle("Calculate", UIControlState.Normal);
+            var resultLabel = new UILabel()
+            {
+                Frame = new CGRect(20, 124, View.Bounds.Width - 40, 40),
+                TextColor = UIColor.Blue,
+                TextAlignment = UITextAlignment.Center,
+                Font = UIFont.SystemFontOfSize(24),
+                Text = "Tip is $0.00",
+            };
+            View.AddSubviews(new UIView[] { totalAmount, calcButton, resultLabel });
+
+            calcButton.TouchUpInside += (s, e) => {
+                double.TryParse(totalAmount.Text, out double value);
+                resultLabel.Text = string.Format("Tip is {0:C}", value * 0.2);
+                totalAmount.ResignFirstResponder();
+            };
+        }
+    }
+}
