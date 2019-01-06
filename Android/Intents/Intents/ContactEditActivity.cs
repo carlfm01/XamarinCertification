55c653537ca3e623517504b7f2c52fce0db3bbf8
Carlos Fonseca M <carlfm01@hotmail.com>
2019-01-06 15:34:58 -0600

    Add Android intents exercise

---
+++ Android/Intents/Intents/ContactEditActivity.cs
@@ -0,0 +1,42 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+
+namespace Intents
+{
+    [Activity(Label = "ContactsActivity")]
+    public class ContactEditActivity : AppCompatActivity
+    {
+        private TextView _userNameTextView;
+        private TextView _emailTextView;
+        private EditText _userNameEditText;
+        private EditText _userEmailEditText;
+        private Button _saveContactButton;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.ContactEdit);
+            _emailTextView = FindViewById<TextView>(Resource.Id.EmailTextView);
+            _userNameTextView = FindViewById<TextView>(Resource.Id.NameTextView);
+            _userNameEditText = FindViewById<EditText>(Resource.Id.PersonNameEditText);
+            _userEmailEditText = FindViewById<EditText>(Resource.Id.EmailEditText);
+            _saveContactButton = FindViewById<Button>(Resource.Id.ButtonSaveContact);
+            _saveContactButton.Click += _saveContactButton_Click;
+            _userNameEditText.Text = Intent.GetStringExtra("Name");
+            _userEmailEditText.Text = Intent.GetStringExtra("Email");
+            // Create your application here
+        }
+
+        private void _saveContactButton_Click(object sender, System.EventArgs e)
+        {
+            var resultIntent = new Intent();
+            resultIntent.PutExtra("Name", _userNameEditText.Text);
+            resultIntent.PutExtra("Email", _userEmailEditText.Text);
+            SetResult(Result.Ok, resultIntent);
+            Finish();
+        }
+    }
+}
---
+++ Android/Intents/Intents/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Widget;
+
+namespace Intents
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button _buttonEditContact;
+        private TextView _userNameTextView;
+        private TextView _emailTextView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+            _buttonEditContact = FindViewById<Button>(Resource.Id.ButtonEditContact);
+            _userNameTextView = FindViewById<TextView>(Resource.Id.UsernameTextView);
+            _emailTextView = FindViewById<TextView>(Resource.Id.EmailTextView);
+            _buttonEditContact.Click += _buttonEditContact_Click;
+
+            _emailTextView.Text = "test@test.test";
+            _userNameTextView.Text = "Carlos";
+        }
+
+        private void _buttonEditContact_Click(object sender, System.EventArgs e)
+        {
+            //StartActivity(typeof(ContactEditActivity));
+            var intent = new Intent(this, typeof(ContactEditActivity));
+            intent.PutExtra("Name", "Carlos");
+            intent.PutExtra("Email", "test@test.test");
+            StartActivityForResult(intent, 506);
+        }
+
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            if (resultCode == Result.Ok)
+            {
+                _emailTextView.Text = data.GetStringExtra("Email");
+                _userNameTextView.Text = data.GetStringExtra("Name");
+            }
+            base.OnActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
