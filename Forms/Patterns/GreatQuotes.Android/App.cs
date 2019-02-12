bd387f6b2d18dc0723ac194f8c896654c86639d9
Carlos Fonseca M <carlfm01@hotmail.com>
2019-02-11 21:56:59 -0600

    Add patterns practice

---
+++ Forms/Patterns/GreatQuotes.Android/App.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using System;
+using Android.Runtime;
+using GreatQuotes.Data;
+
+namespace GreatQuotes
+{
+    [Application(Icon = "@drawable/icon", Label = "@string/app_name")]
+    public class App : Application
+    {
+        private readonly SimpleContainer _container = new SimpleContainer();
+
+        public App(IntPtr h, JniHandleOwnership jho) : base(h, jho)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            //QuoteLoaderFactory.Create = () => new QuoteLoader();
+            ServiceLocator.Instance.Add<ITextToSpeech, TextToSpeechService>();
+            _container.Register<IQuoteLoader, QuoteLoader>();
+            _container.Create<QuoteManager>();
+        }
+
+        public static void Save() => QuoteManager.Instance.Save();
+    }
+}
+
---
+++ Forms/Patterns/GreatQuotes.Data/QuoteManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace GreatQuotes.Data
+{
+    public class QuoteManager
+    {
+        public static QuoteManager Instance { get; private set; }
+
+        readonly IQuoteLoader _loader;
+        public IList<GreatQuote> Quotes { get; private set; }
+
+        public QuoteManager(IQuoteLoader loader)
+        {
+            if (Instance != null)
+            {
+                throw new Exception("Can only create a single QuoteManager.");
+            }
+            Instance = this;
+            _loader = loader;
+            Quotes = new ObservableCollection<GreatQuote>(loader.Load());
+        }
+        //static readonly Lazy<QuoteManager> instance = new Lazy<QuoteManager>(() => new QuoteManager());
+        //public static QuoteManager Instance { get { return instance.Value; } }
+
+        //readonly IQuoteLoader repo;
+        //public IList<GreatQuote> Quotes { get; private set; }
+
+        //private QuoteManager()
+        //{
+        //    repo = QuoteLoaderFactory.Create();
+        //    Quotes = new ObservableCollection<GreatQuote>(repo.Load());
+        //}
+
+        public void Save()
+        {
+            _loader.Save(Quotes);
+        }
+
+        public void SayQuote(GreatQuote quote)
+        {
+            if (quote == null)
+                throw new ArgumentNullException("quote");
+
+            ITextToSpeech tts = ServiceLocator.Instance.Resolve<ITextToSpeech>();
+
+            var text = quote.QuoteText;
+
+            if (!string.IsNullOrWhiteSpace(quote.Author))
+                text += $" by {quote.Author}";
+
+            tts.Speak(text);
+        }
+    }
+}
